5e6f417f19d4c21fb9fb6977f9410d151570052e
sezyu <129446663+sezyu@users.noreply.github.com>
2026-03-06 02:00:56 +0400

    Update task3.sql

--- sql/task3.sql
+++ sql/task3.sql
@@ -1,3 +1,22 @@
+Задание 3: Покупки
+create table account
+(
+    id integer, -- ID счета
+    client_id integer, -- ID клиента
+    open_dt date, -- дата открытия счета
+    close_dt date -- дата закрытия счета
+)
+
+create table transaction
+(
+    id integer,  -- ID транзакции
+    account_id integer,  -- ID счета
+    transaction_date date,  -- дата транзакции
+    amount numeric(10,2), -- сумма транзакции
+    type varchar(3) -- тип транзакции
+)
+Вывести ID клиентов, которые за последний месяц по всем своим счетам совершили покупок меньше, чем на 5000 рублей.
+
 SELECT
     account.client_id
 FROM
